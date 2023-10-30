86355f01dd818693cf9c3a0d2af4bee653630bdf
priyadarshii786 <tathagatp918@gmail.com>
2023-10-30 14:29:57 +0530

    Add ability to draw

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import MenuReducer from '@/slice/menuSlice'
 import ToolboxReducer from '@/slice/toolboxSlice'
 
+
 export const store = configureStore({
     reducer: {
         menu: MenuReducer,
